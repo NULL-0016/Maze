328a95dd12de2728badb439ee7d8abee7eb8a0fe
NULL-0016 <sergantnull@gmail.com>
2026-02-12 17:16:22 +0300

    RenderWindow class update.

--- RenderWindow.cpp
+++ RenderWindow.cpp
@@ -1,1 +1,34 @@
 #include "RenderWindow.h"
+
+bool RenderWindow::Initialize(HINSTANCE hInstance, std::string w_title, std::string w_class, int width, int height)
+{
+	this->hInstance = hInstance;
+	this->width = width;
+	this->height = height;
+	this->window_title = w_title;
+	this->window_title_wide = StringConverter::StringToWide(w_title);
+	this->window_class = w_class;
+	this->window_class_wide = StringConverter::StringToWide(w_class);
+
+	this->RegisterWindowClass();
+
+	return false;
+}
+
+void RenderWindow::RegisterWindowClass()
+{
+	WNDCLASSEX wc;
+	wc.style = CS_HREDRAW | CS_VREDRAW | CS_OWNDC;
+	wc.lpfnWndProc = DefWindowProc;
+	wc.cbClsExtra = 0;
+	wc.cbWndExtra = 0;
+	wc.hInstance = this->hInstance;
+	wc.hIcon = NULL;
+	wc.hIconSm = NULL;
+	wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wc.hbrBackground = NULL;
+	wc.lpszMenuName = NULL;
+	wc.lpszClassName = this->window_class_wide.c_str();
+	wc.cbSize = sizeof(WNDCLASSEX);
+	RegisterClassEx(&wc);
+}
